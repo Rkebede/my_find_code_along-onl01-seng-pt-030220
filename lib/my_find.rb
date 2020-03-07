df1a5b7880d90bac271c1747d30a739b6d748b70
Ruth Kebede <rutht.kebede@gmail.com>
2020-03-07 06:18:08 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,13 @@
 require 'pry'
 
 def my_find(collection)
-
+  i = 0 
+  while i < collection.length 
+  return
+   if yield(collection[i])
+     return collection[i]
+   end 
+    i += 1 
+  end
+  
 end
